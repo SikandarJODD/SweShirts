e1d3403964a58329a2009780a53cda5045ee5883
SikandarJODD <vu1f2021072@pvppcoe.ac.in>
2023-02-26 01:07:47 +0530

    Adedd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,15 @@
 <h1 class="text-3xl font-bold text-center text-slate-200">Fusion XD</h1>
 
+<svelte:head>
+	<title>Sweart Shirts</title>
+	<meta name="description" content="Sweatshirts For Men" />
+	<meta
+		name="og:image"
+		itemprop="image"
+		content="https://cdn-icons-png.flaticon.com/512/2806/2806045.png"
+	/>
+</svelte:head>
+
 <style lang="postcss">
 	:global(html) {
 		/* background-color: theme(colors.gray.100) !important; */
