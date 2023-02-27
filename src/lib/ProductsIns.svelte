2b481dd9e5f30e83cc05fc68c7feff21974f5b41
SikandarJODD <vu1f2021072@pvppcoe.ac.in>
2023-02-27 11:20:03 +0530

    Stuff

--- src/lib/ProductsIns.svelte
+++ src/lib/ProductsIns.svelte
@@ -1,12 +1,12 @@
 <script>
-	import Img1 from '$lib/images/img1.png';
-	import Img2 from '$lib/images/img2.jpg';
-	import Img3 from '$lib/images/img3.jpg';
-	import Img4 from '$lib/images/img4.jpg';
-	import Img5 from '$lib/images/img5.jpg';
-	import Img6 from '$lib/images/img6.jpg';
-	import Img7 from '$lib/images/img7.jpg';
-	import Img8 from '$lib/images/img8.jpg';
+	import Img1 from '$lib/Images/img1.png';
+	import Img2 from '$lib/Images/img2.jpg';
+	import Img3 from '$lib/Images/img3.jpg';
+	import Img4 from '$lib/Images/img4.jpg';
+	import Img5 from '$lib/Images/img5.jpg';
+	import Img6 from '$lib/Images/img6.jpg';
+	import Img7 from '$lib/Images/img7.jpg';
+	import Img8 from '$lib/Images/img8.jpg';
 	import ImgMap from './ImgMap.svelte';
 	let imgData = [
 		{
@@ -52,6 +52,7 @@
 	];
 </script>
 
+<!-- Tiffu -->
 <div class=" py-6 sm:py-2 lg:py-1 mb-20">
 	<div class="max-w-screen-xl px-4 md:px-8 mx-auto">
 		<div class="grid grid-cols-2 md:grid-cols-3 gap-x-4 lg:gap-x-8 gap-y-8 lg:gap-y-12">
