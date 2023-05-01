be5b0adc172ffb83730bde252036a80af000c9f3
SikandarJODD <vu1f2021072@pvppcoe.ac.in>
2023-05-01 18:50:15 +0530

    Added stuff

--- src/lib/Aboutcontent.svelte
+++ src/lib/Aboutcontent.svelte
@@ -1,68 +1,155 @@
 <script>
-	import AOS from 'aos';
-	import 'aos/dist/aos.css'; // You can also use <link> for styles
-	// ..
-	AOS.init();
+	import LogoAditya from '$lib/aditya.png';
 </script>
 
-<div
-	class="bg-white py-6 sm:py-8 lg:py-12"
-	data-aos="zoom-in"
-	data-aos-offset="100"
-	data-aos-delay="10"
->
+<div class="bg-white py-6 sm:py-8 lg:py-12">
 	<div class="mx-auto max-w-screen-xl px-4 md:px-8">
-		<div class="grid gap-8 md:grid-cols-2 lg:gap-12">
-			<div>
-				<div class="h-64 overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-auto">
+		<!-- text - start -->
+		<div class="mb-10 md:mb-16">
+			<h2 class="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">
+				Meet our Team
+			</h2>
+
+			<p class="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">
+				This is a section of some simple filler text, also known as placeholder text. It shares some
+				characteristics of a real written text but is random or otherwise generated.
+			</p>
+		</div>
+		<!-- text - end -->
+
+		<div class="flex justify-center   ">
+			<!-- person - start -->
+			<div class="flex flex-col items-center mx-10">
+				<div
+					class="mb-2 h-24 w-24 overflow-hidden rounded-full bg-gray-100 shadow-lg md:mb-4 md:h-32 md:w-32"
+				>
+					<!-- svelte-ignore a11y-img-redundant-alt -->
 					<img
-						src="https://images.unsplash.com/photo-1610465299996-30f240ac2b1c?auto=format&q=75&fit=crop&w=600&h=750"
+						src="https://media.licdn.com/dms/image/C4D03AQGP1dNhTLkZrA/profile-displayphoto-shrink_400_400/0/1662137583464?e=1688601600&v=beta&t=s1O-HJewYvYjv5zsBci2w0i1OLHgo8bJCzh76044DXE"
 						loading="lazy"
-						alt="Photo by Martin Sanchez"
+						alt="Photo by Radu Florin"
 						class="h-full w-full object-cover object-center"
 					/>
 				</div>
+
+				<div>
+					<div class="text-center font-bold text-indigo-500 md:text-lg">Sikandar.S.Bhide</div>
+					<p class="mb-3 text-center text-sm text-gray-500 md:mb-4 md:text-base">
+						Coder + Designer
+					</p>
+
+					<!-- social - start -->
+					<div class="flex justify-center">
+						<div class="flex gap-4">
+							<a
+								href="https://www.linkedin.com/in/sikandar-bhide-911988211/"
+								target="_blank"
+								class="text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600"
+							>
+								<svg
+									class="h-5 w-5"
+									width="24"
+									height="24"
+									viewBox="0 0 24 24"
+									fill="currentColor"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"
+									/>
+								</svg>
+							</a>
+
+							<a
+								href="https://www.linkedin.com/in/sikandar-bhide-911988211/"
+								target="_blank"
+								class="text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600"
+							>
+								<svg
+									class="h-5 w-5"
+									width="24"
+									height="24"
+									viewBox="0 0 24 24"
+									fill="currentColor"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"
+									/>
+								</svg>
+							</a>
+						</div>
+					</div>
+					<!-- social - end -->
+				</div>
 			</div>
+			<!-- person - end -->
 
-			<div class="md:pt-8">
-				<p class="text-center font-bold text-indigo-500 md:text-left">Who we are</p>
-
-				<h1
-					class="mb-4 text-center text-2xl font-bold text-gray-800 sm:text-3xl md:mb-6 md:text-left"
+			<!-- person - start -->
+			<div class="flex flex-col items-center mx-10">
+				<div
+					class="mb-2 h-24 w-24 overflow-hidden rounded-full bg-gray-100 shadow-lg md:mb-4 md:h-32 md:w-32"
 				>
-					Our competitive advantage
-				</h1>
+					<img
+						src={LogoAditya}
+						loading="lazy"
+						alt="Photo by christian ferrer"
+						class="h-full w-full object-cover object-center"
+					/>
+				</div>
 
-				<p class="mb-6 text-gray-500 sm:text-lg md:mb-8">
-					This is a section of some simple filler text, also known as placeholder text. It shares
-					some characteristics of a real written text but is random or otherwise generated. It may
-					be used to display a sample of fonts or generate text for testing. Filler text is dummy
-					text which has no meaning however looks very similar to real text.<br /><br />
+				<div>
+					<div class="text-center font-bold text-indigo-500 md:text-lg">Aditya Karle</div>
+					<p class="mb-3 text-center text-sm text-gray-500 md:mb-4 md:text-base">
+						Backend Developer + System Design
+					</p>
 
-					This is a section of some simple filler text, also known as placeholder text. It shares
-					some characteristics of a real written text but is
-					<a
-						href="#"
-						class="text-indigo-500 underline transition duration-100 hover:text-indigo-600 active:text-indigo-700"
-						>random</a
-					> or otherwise generated. It may be used to display a sample of fonts or generate text for
-					testing. Filler text is dummy text which has no meaning however looks very similar to real
-					text.
-				</p>
+					<!-- social - start -->
+					<div class="flex justify-center">
+						<div class="flex gap-4">
+							<a
+								href="#"
+								target="_blank"
+								class="text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600"
+							>
+								<svg
+									class="h-5 w-5"
+									width="24"
+									height="24"
+									viewBox="0 0 24 24"
+									fill="currentColor"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"
+									/>
+								</svg>
+							</a>
 
-				<h2
-					class="mb-2 text-center text-xl font-semibold text-gray-800 sm:text-2xl md:mb-4 md:text-left"
-				>
-					About us
-				</h2>
-
-				<p class="mb-6 text-gray-500 sm:text-lg md:mb-8">
-					This is a section of some simple filler text, also known as placeholder text. It shares
-					some characteristics of a real written text but is random or otherwise generated. It may
-					be used to display a sample of fonts or generate text for testing. Filler text is dummy
-					text which has no meaning however looks very similar to real text.
-				</p>
+							<a
+								href="#"
+								target="_blank"
+								class="text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600"
+							>
+								<svg
+									class="h-5 w-5"
+									width="24"
+									height="24"
+									viewBox="0 0 24 24"
+									fill="currentColor"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"
+									/>
+								</svg>
+							</a>
+						</div>
+					</div>
+					<!-- social - end -->
+				</div>
 			</div>
+			<!-- person - end -->
 		</div>
 	</div>
 </div>
